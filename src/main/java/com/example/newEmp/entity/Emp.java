d1db6661bf3579cceac1b051a8052d35d8c7ec09
surajitp66 <surajitp66@gmail.com>
2023-01-24 14:36:30 +0530

    Initial Version 2.1 in copy

--- src/main/java/com/example/newEmp/entity/Emp.java
+++ src/main/java/com/example/newEmp/entity/Emp.java
@@ -8,8 +8,8 @@ import lombok.Setter;
 
 /*import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;*/
-//import javax.validation.constraints.Min;
-//import javax.validation.constraints.NotNull;
+/*import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;*/
 
 @Getter
 @Setter
